306149591649588152d0a4bda352b471d005bc72
Stanley Yu <yu.stanley@yahoo.com>
2021-04-18 21:13:11 -0400

    optimize docker image creation

---
+++ etc/init_jupyter.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# generate configuration, cert, and password if this is the first run
+#
+if [ ! -f /var/tmp/zipline_init ] ; then
+    jupyter notebook --allow-root --generate-config
+    if [ ! -f ${SSL_CERT_PEM} ] ; then
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=127.0.0.1" \
+            -keyout ${SSL_CERT_KEY} -out ${SSL_CERT_PEM}
+    fi
+    echo "c.NotebookApp.password = ${PW_HASH}" >> ${CONFIG_PATH}
+    touch /var/tmp/zipline_init
+fi
